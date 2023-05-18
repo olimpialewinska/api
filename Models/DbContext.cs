947ca8e69ab568a61267f3d1ec97a6d8c7b0ff6f
Olimpia <o.lewinska.336@studms.ug.edu.pl>
2023-05-18 19:16:18 +0200

    Initial commit

---
+++ Models/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+public class InMemoryDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseInMemoryDatabase(databaseName: "UsersDb");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskModel>().Property(e => e.Id).ValueGeneratedNever();
+    }
+
+    public DbSet<UserModel> Users{ get; set; }
+}
+
---
+++ Models/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public virtual List<TaskModel> Tasks { get; set; }
+    }
+
+    public class TaskModel
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
